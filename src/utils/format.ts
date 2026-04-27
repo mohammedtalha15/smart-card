19504c1d8defa750077451c39466ec165c1b8f43
mohammedtalha15 <mohammedtalha1514@gmail.com>
2026-04-27 23:09:12 +0530

    feat: add currency formatting utility

---
+++ src/utils/format.ts
@@ -0,0 +1,6 @@
+export const formatCurrency = (amount: number, currency: string = 'INR') => {
+  return new Intl.NumberFormat('en-IN', {
+    style: 'currency',
+    currency,
+  }).format(amount);
+};
